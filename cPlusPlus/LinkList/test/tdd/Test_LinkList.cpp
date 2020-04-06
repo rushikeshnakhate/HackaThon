db8fa2ab22afeff9b81f569b3323e5461cddd23e
rushikeshnakhate <rushikeshnakhate@gmail.com>
2020-04-06 22:09:15 +0800

    Link List in C++ #7

--- cPlusPlus/LinkList/test/tdd/Test_LinkList.cpp
+++ cPlusPlus/LinkList/test/tdd/Test_LinkList.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <LinkList.h>
 #include "Test_LinkList.h"
 
-TEST_F(Test_LinkList, testing) {
-    EXPECT_EQ(0, 0);
+class Test_LinkList : public ::testing::Test {
+
+public:
+    std::unique_ptr<LinkList> pNode;
+
+    void SetUp() override {
+        pNode = std::make_unique<LinkList>();
+    }
+
+    void TearDown() override {
+        if (pNode)
+            std::cout << "smart pointer is finished" << std::endl;
+    }
+};
+
+
+TEST_F(Test_LinkList, when_get_number_of_is_called_then_total_number_of_nodes_returned) {
+    EXPECT_EQ(pNode->get_number_of_nodes(), -1);
+}
+
+TEST_F(Test_LinkList, when_new_data_added_then_linklist_add_it_to_node) {
+    pNode->add_node(10);
+
 }
 
 int mai(int argc, char **argv) {
