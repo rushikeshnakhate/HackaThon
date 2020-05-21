f8660e0ed7b95816d3ca8611a6755810f818b3e7
rushikeshnakhate <rushikeshnakhate@gmail.com>
2020-05-22 07:48:11 +0800

    Sub Array with given Sum #13

--- cPlusPlus/SubArrayWithGivenSum/test/Test_ArrayWithGivenSum.cpp
+++ cPlusPlus/SubArrayWithGivenSum/test/Test_ArrayWithGivenSum.cpp
@@ -5,6 +5,6 @@ TEST_F(Test_ArrayWithGivenSum, get_index_of_arrays_for_give_sum) {
     int sum = 12;
     std::tuple<int, int> result;
     std::tuple<int, int> expected{1, 3};
-    EXPECT_EQ(arrayWithGivenSum_.get_sub_array_index(sum, input_array, result), 1);
+    EXPECT_TRUE(arrayWithGivenSum_.get_sub_array_index(sum, input_array, result));
     EXPECT_EQ(result, expected);
 }
