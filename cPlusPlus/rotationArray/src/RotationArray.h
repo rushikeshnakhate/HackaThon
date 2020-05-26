6b87968a1738cd4ccc0b8fc5380448a5feaabc51
rushikeshnakhate <rushikeshnakhate@gmail.com>
2020-05-26 14:33:05 +0800

    array rotation #15

---
+++ cPlusPlus/rotationArray/src/RotationArray.h
@@ -0,0 +1,17 @@
+//
+// Created by ayush on 5/26/2020.
+//
+
+#ifndef HACKTON_ROTATIONARRAY_H
+#define HACKTON_ROTATIONARRAY_H
+
+#include <vector>
+#include <algorithm>
+
+class RotationArray {
+public:
+    std::vector<int> rotate(std::vector<int> &V,int & attempt );
+};
+
+
+#endif //HACKTON_ROTATIONARRAY_H
---
+++ cPlusPlus/rotationArray/test/Test_RotationArray.h
@@ -0,0 +1,22 @@
+//
+// Created by ayush on 5/26/2020.
+//
+
+#ifndef HACKTON_TEST_ROTATIONARRAY_H
+#define HACKTON_TEST_ROTATIONARRAY_H
+
+
+#include <gtest/gtest.h>
+#include "../src/RotationArray.h"
+
+class Test_RotationArray : public ::testing::Test {
+public:
+    RotationArray rotationArray_;
+protected:
+    void SetUp() override {};
+
+    void TearDown() override {};
+};
+
+
+#endif //HACKTON_TEST_ROTATIONARRAY_H
